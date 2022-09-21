a2229819f150ef51bc8d08dc13e0dc84edfdd45d
AnnaHelene01 <91527942+AnnaHelene01@users.noreply.github.com>
2022-09-21 14:52:56 +0200

    Update module.mjs

--- Modul 3.2/module.mjs
+++ Modul 3.2/module.mjs
@@ -2,7 +2,6 @@ export function addNumbers() {
     console.log('This is the addNumber function')
 };
 
-
 //export default calculate() {
   //  console.log('This is the calculate function from module.mjs');
 //};
